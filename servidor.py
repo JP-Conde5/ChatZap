432589ab14a900ca6f32ae123e1e194fe3091724
JP-Conde5 <jcalves25@homail.com>
2025-01-21 14:31:53 -0300

    Testando

--- servidor.py
+++ servidor.py
@@ -1,25 +1,24 @@
 #Fazer o ngc de página
 import socket
 import tkinter as tk
-import sys
-def servidor(master):
-    print("Na escuta, aguardando alguém conectar :)")
+from threading import Thread
 
+def servidor():
+    janela_servidor = tk.Toplevel()
+    janela_servidor.geometry("800x600")
     HOST = socket.gethostbyname_ex(socket.gethostname())[2][0] #Onde será executad0o
     PORT = 65432 #Porta que será solicitada ao SO, quando for executado
     linha = 0
-    tk.Label(master, text=f"Seu ip é:, {HOST}").grid(row=linha)
-
+    tk.Label(janela_servidor, text=f"Seu ip é:, {HOST}").grid(row=linha)
     linha += 1
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_server:
         socket_server.bind((HOST, PORT)) #bind faz a solicitação da porta ao SO
         socket_server.listen()
-        tk.Label(master, text="Na escuta, aguardando alguém conectar :)").grid(row=linha)
+        tk.Label(janela_servidor, text="Na escuta, aguardando alguém conectar :)").grid(row=linha)
         linha += 1
         connection, address = socket_server.accept() #Função que aceita a requisição de conexão que retorna uma tupla com dois valores: o objeto que representa conexão (envia e recebe) e o endereço IP do cliente
         with connection:
-            tk.Label(master, text=f"Conexão feito com {address}").grid(row=linha)
-
+            tk.Label(janela_servidor, text=f"Conexão feito com {address}").grid(row=linha)
             while True:
                 linha += 1
                 coluna = 0
@@ -27,49 +26,58 @@ def servidor(master):
                 dados = dados.decode() #Como recv retorna bytes, essa função decodifica esses dados
                 if not dados: #Se dados nulos
                     break
-                tk.Label(master, text=f"Cliente: {dados}. Quer enviar de volta? [s]/[n]").grid(row=linha, column=coluna)
+                tk.Label(janela_servidor, text=f"Cliente: {dados}. Quer enviar de volta? [s]/[n]").grid(row=linha, column=coluna)
                 coluna += 1
-                resp = tk.Entry(master).grid(row=linha,column=coluna)
+                resp = tk.Entry(janela_servidor).grid(row=linha,column=coluna)
                 coluna = 0
                 linha += 1
-                if resp.lower() == "s":
-                    tk.Label(master, text=f"Sua mensagem:").grid(row=linha, column=coluna)
+                if resp.get().lower() == "s":
+                    tk.Label(janela_servidor, text=f"Sua mensagem:").grid(row=linha, column=coluna)
                     coluna += 1
-                    resp = tk.Entry(master).grid(row=linha, column=coluna)
-                    connection.sendall(resp.encode())
-                elif resp.lower() == 'n':
-                    connection.sendall(resp.encode()) #envia para todos conectados
+                    resp = tk.Entry(janela_servidor).grid(row=linha, column=coluna)
+                    connection.sendall(resp.get().encode())
+                elif resp.get().lower() == 'n':
+                    connection.sendall(resp.get().encode()) #envia para todos conectados
                     break
-        tk.mainloop()
+    janela_servidor.destroy()
 
-def cliente(master):
-    tk.Label(master, text="Digite o IP DO SERVIDOR: ").grid(row=0)
-    HOST = tk.Entry(master).grid(row=0, column=1) #IP da máquina que está o servidor
+def cliente():
+    janela_cliente = tk.Toplevel()
+    linha = 0
+    tk.Label(janela_cliente, text="Digite o IP DO SERVIDOR: ").grid(row=linha)
+    HOST = tk.Entry(janela_cliente).grid(row=0, column=1) #IP da máquina que está o servidor
+    HOST = HOST.get()
     PORT = 65432 #Porta que o cliente estará ouvindo 
-
+    linha += 1
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_client:
         socket_client.connect((HOST, PORT)) #Função que busca conectar com um servidor
-        print("Conectado ao servidor")
+        tk.Label(master, text="Conectado ao servidor").grid(row=linha)
+        linha += 1
         while True:
-            msg = input("Digite sua mensagem: ").encode()
+            coluna = 0
+            tk.Label(master, text="Sua mensagem: ").grid(row=linha, column=coluna)
+            coluna += 1
+            msg = tk.Entry(master).grid(row=linha, column=coluna)
+            coluna = 0
+            linha += 1
             socket_client.sendall(msg) 
-            if msg.lower() == 'sair':
+            if msg.get().lower() == 'sair':
                 resp = socket_client.recv(1024).decode()
-                print(f"{resp}")
+                tk.Label(master, text=f"{resp}").grid(row=linha, column=coluna)
                 break
             resp = socket_client.recv(1024).decode()
             if resp == 'n':
                 break
-            print(f"{resp}")
+            tk.Label(master, text=f"{resp}").grid(row=linha, column=coluna)
+    janela_cliente.destroy()
     print("Conexão encerrada")
-    tk.mainloop()
     #TCP entrega pacotes em ordem, enquanto UDP não
     
-def pos_escolha(master):
+def pos_escolha():
     if escolha.get().lower() == 's':
-        servidor(master)
+        Thread(target=servidor, daemon=True).start()  # Executa servidor em uma thread
     elif escolha.get().lower == 'c':
-        cliente(master)
+        Thread(target=cliente, daemon=True).start()  # Executa servidor em uma thread
 
 
 master = tk.Tk()
@@ -78,7 +86,7 @@ tk.Label(master, text="Você é o servidor [S] ou cliente [C]: ").grid(row=0)
 master.title('ChatZap')
 escolha = tk.Entry(master)
 escolha.grid(row=0, column=1)
-tk.Button(master, text='Entre', command=pos_escolha(master)).grid(row=2, column=0, sticky=tk.W,pady=4)
+tk.Button(master, text='Entre', command=pos_escolha).grid(row=2, column=0, sticky=tk.W,pady=4)
 tk.mainloop()
 
 
